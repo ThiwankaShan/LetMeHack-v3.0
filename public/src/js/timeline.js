4997001d44a6ecbe2f3d737f8e12f2627e778b4e
ThiwankaShan <shane.thiwanka@gmail.com>
2023-06-19 01:24:53 +0530

    fix styles timeline, gradient, about sections

--- public/src/js/timeline.js
+++ public/src/js/timeline.js
@@ -1,29 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
-var items = document.querySelectorAll("li");
+// document.addEventListener("DOMContentLoaded", function() {
+// var items = document.querySelectorAll("li");
 
-function isItemInView(item){
-  var rect = item.getBoundingClientRect();
-  return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
+// function isItemInView(item){
+//   var rect = item.getBoundingClientRect();
+//   return (
+//       rect.top >= 0 &&
+//       rect.left >= 0 &&
+//       rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+//       rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+//     );
+// }
 
-function callbackFunc() {
-    // console.log('called');
-    console.log(items);
+// function callbackFunc() {
+//     // console.log('called');
+//     console.log(items);
 
-    for (var i = 0; i < items.length; i++) {
-      if (isItemInView(items[i])) {
-        items[i].classList.add("show");
-      }
-    }
-  }
+//     for (var i = 0; i < items.length; i++) {
+//       if (isItemInView(items[i])) {
+//         items[i].classList.add("show");
+//       }
+//     }
+//   }
 
-  // listen for events
-  window.addEventListener("load", callbackFunc);
-  window.addEventListener("resize", callbackFunc);
-  window.addEventListener("scroll", callbackFunc);
-});
+//   // listen for events
+//   window.addEventListener("load", callbackFunc);
+//   window.addEventListener("resize", callbackFunc);
+//   window.addEventListener("scroll", callbackFunc);
+// });
