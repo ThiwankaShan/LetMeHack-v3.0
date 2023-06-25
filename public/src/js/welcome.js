0e600e867a5087edfa539b6fd85162b8593bd9ce
Shan <shane.thiwanka@gmail.com>
2023-06-26 00:05:30 +0530

    release v0.1 (#20) (#21)
    * Update firebase-hosting-merge.yml
    
    * Update firebase-hosting-merge.yml
    
    * test pipeline
    
    * test pipeline
    
    * Update README.md
    
    Add hosted site URL
    
    * update folder structure
    
    * Create LICENSE
    
    * add bootstrap lib
    
    * add about section
    
    * modify the about section
    
    * change icons
    
    * setup navbar and welcome section
    
    * add changes to navbar
    
    * added timeline section
    
    * added timeline section
    
    * add changes to navbar toggler
    
    * remove inline style in navbar toggler
    
    * add new changes and custom togger to navbar
    
    * Added contact us section
    
    * added timeline to index
    
    * removed duplicated bootstrap import
    
    * modified styles
    
    * fix styles timeline, gradient, about sections
    
    * update banner and logo imgs
    
    * update awards section
    
    * update welcome and about sections
    
    * add sponsor section
    
    * update contact us section
    
    * add oganized by section
    
    * add timeline section
    
    * update particle js animation
    
    * add footer and setup parallax animation lib
    
    * add parallax to welcome section
    
    * update abut
    
    * code clean up and update UI
    
    * fix fonts
    
    * update alignments and logos
    
    * add smooth scroll and update welcome parallax
    
    * code clean up
    
    ---------
    
    Co-authored-by: Kobzpik <prabath.pik2299@gmail.com>
    Co-authored-by: Thimira97 <thimiramadusanka1997@gmail.com>
    Co-authored-by: nadunMadhushanka <112070908+nadunMadhushanka@users.noreply.github.com>
    Co-authored-by: UmeshaNirmani <umeshanirmani@gmail.com>
    Co-authored-by: Prabath Indrajith <57173720+Kobzpik@users.noreply.github.com>

---
+++ public/src/js/welcome.js
@@ -0,0 +1,23 @@
+let img5 = document.getElementById("image-5");
+let img4 = document.getElementById("image-4");
+let img3 = document.getElementById("image-3");
+let img2 = document.getElementById("image-2");
+let img1 = document.getElementById("image-1");
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+
+    // img5.style.top = value * -0.5 + 'px';
+    img4.style.top = value * 0.3 + 'px';
+    img3.style.top = value * 0.2 + 'px';
+    img2.style.top = value * 0.1 + 'px';
+    // img1.style.top = value * -0.4 + 'px';
+    
+    // Apply smooth transition to images
+    img4.style.transition = 'top 0.4s ease';
+    img3.style.transition = 'top 0.4s ease';
+    img2.style.transition = 'top 0.4s ease';
+});
+
+
+
