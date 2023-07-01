c336aeae52ccb5f94d4cdc74e78a8364d8cc52f4
ThiwankaShan <shane.thiwanka@gmail.com>
2023-07-01 15:58:25 +0530

    code clean up index.js

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -5,25 +5,3 @@ document.addEventListener("DOMContentLoaded", function() {
         waitUntilVisible: true
     }).go();
 });
-
-
-// Smooth scrolling (ease the scrolling)
-const body = document.body,
-    scrollWrap = document.getElementsByClassName("smooth-scroll-wrapper")[0],
-    height = scrollWrap.getBoundingClientRect().height - 1,
-    speed = 0.04;
-
-var offset = 0;
-
-body.style.height = Math.floor(height) + "px";
-
-function smoothScroll() {
-    offset += (window.pageYOffset - offset) * speed;
-
-    var scroll = "translateY(-" + offset + "px) translateZ(0)";
-    scrollWrap.style.transform = scroll;
-
-    callScroll = requestAnimationFrame(smoothScroll);
-}
-
-smoothScroll();
